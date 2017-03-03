989e383ce0d0153f58658d01771f16ee8c3bd74b
meganhmoore <mm7148@nyu.edu>
2017-03-03 17:18:52 -0500

    Add files via upload

--- Piezo.ino
+++ Piezo.ino
@@ -5,11 +5,13 @@
 BLEPeripheral blePeripheral;
 
 // create service
-BLEService piezoService = BLEService("FF10");
+BLEService piezoService = BLEService("5FA0");
 
-// create switch and dimmer characteristic
-BLEUnsignedCharCharacteristic piezoSongCharacteristic = BLEUnsignedCharCharacteristic("FF11", BLERead | BLEWrite);
-BLEDescriptor piezoSongDescriptor = BLEDescriptor("2901", "Dimmer");
+// create piezo characteristic
+BLEUnsignedCharCharacteristic loudnessCharacteristic = BLEUnsignedCharCharacteristic("5FA1", BLERead | BLEWrite);
+BLEDescriptor loudnessDescriptor = BLEDescriptor("2901", "Loudness");
+BLECharCharacteristic switchCharacteristic = BLECharCharacteristic("FF11", BLERead | BLEWrite);
+BLEDescriptor switchDescriptor = BLEDescriptor("2901", "Switch");
 
 void setup() {
   Serial.begin(9600);
@@ -24,11 +26,14 @@ void setup() {
 
   // add service and characteristics
   blePeripheral.addAttribute(piezoService);
-  blePeripheral.addAttribute(piezoSongCharacteristic);
-  blePeripheral.addAttribute(piezoSongDescriptor);
+  blePeripheral.addAttribute(loudnessCharacteristic);
+  blePeripheral.addAttribute(loudnessDescriptor);
+  blePeripheral.addAttribute(switchCharacteristic);
+  blePeripheral.addAttribute(switchDescriptor);
 
   // assign event handlers for characteristic
-  piezoSongCharacteristic.setEventHandler(BLEWritten, piezoSongCharacteristicWritten);
+  loudnessCharacteristic.setEventHandler(BLEWritten, loudnessCharacteristicWritten);
+  switchCharacteristic.setEventHandler(BLEWritten, switchCharacteristicWritten);
 
   // begin initialization
   blePeripheral.begin();
@@ -41,24 +46,38 @@ void loop() {
   blePeripheral.poll();
 }
 
-void piezoSongCharacteristicWritten(BLECentral& central, BLECharacteristic& characteristic) {
+void loudnessCharacteristicWritten(BLECentral& central, BLECharacteristic& characteristic) {
   Serial.print(F("Dimmer set to: "));
-  Serial.println(piezoSongCharacteristic.value());
-  analogWrite(PIEZO_PIN, piezoSongCharacteristic.value());
-  piezoSong(central);
+  Serial.println(loudnessCharacteristic.value());
+  analogWrite(PIEZO_PIN, loudnessCharacteristic.value());
+  //piezoSong();
+}
+void switchCharacteristicWritten(BLECentral& central, BLECharacteristic& characteristic) {
+  // central wrote new value to characteristic, update PIEZO
+  Serial.print(F("Characteristic event, written: "));
+
+  if (switchCharacteristic.value()) {
+    Serial.println(F("PIEZO on"));
+    analogWrite(PIEZO_PIN, 0xFF);
+  } else {
+    Serial.println(F("PIEZO off"));
+    analogWrite(PIEZO_PIN, 0x00);
+  }
+
 }
 
-void piezoSong(BLECentral& central){
+
+void piezoSong(){
   for(int i=0;i<8;i++){
     analogWrite(PIEZO_PIN, 0);
     delay(250);
-    analogWrite(PIEZO_PIN, piezoSongCharacteristic.value()-(i*30));
+    analogWrite(PIEZO_PIN, loudnessCharacteristic.value()-(i*30));
     delay(250);
     analogWrite(PIEZO_PIN, 0);
     delay(250);
-    analogWrite(PIEZO_PIN, piezoSongCharacteristic.value()-(i*30));
+    analogWrite(PIEZO_PIN, loudnessCharacteristic.value()-(i*30));
     delay(250);
-    analogWrite(PIEZO_PIN, piezoSongCharacteristic.value()-(i*30));
+    analogWrite(PIEZO_PIN, loudnessCharacteristic.value()-(i*30));
   }
   analogWrite(PIEZO_PIN, 0);
 }
